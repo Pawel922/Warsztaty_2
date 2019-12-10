16ed12db3079157b0f4e52c5cd9eb2087dcfa5dd
Pawel Wozniak <pawel.jan.wozniak@gmail.com>
2019-12-10 23:30:45 +0100

    File Group.java was added.

---
+++ src/pl/coderslab/warsztaty_2/models/Group.java
@@ -0,0 +1,30 @@
+package pl.coderslab.warsztaty_2.models;
+
+public class Group {
+
+    private int id;
+    private String name;
+
+    public Group() {}
+
+    public Group(String name){
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
