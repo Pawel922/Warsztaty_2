5dc6afecc9d056b43fba5dddd2fdd181802096f3
Pawel Wozniak <pawel.jan.wozniak@gmail.com>
2019-12-13 22:25:50 +0100

    File SolutionDao.java was added.

---
+++ src/pl/coderslab/warsztaty_2/daos/SolutionDao.java
@@ -0,0 +1,116 @@
+package pl.coderslab.warsztaty_2.daos;
+
+import pl.coderslab.warsztaty_2.DBUtil;
+import pl.coderslab.warsztaty_2.models.Solution;
+import java.sql.*;
+import java.util.Arrays;
+
+
+public class SolutionDao {
+
+    private static final String CREATE_SOLUTION_QUERY =
+            "INSERT INTO solutions(created, updated, description, exercise_id, user_id) VALUES (?, ?, ?, ?, ?)";
+    private static final String READ_SOLUTION_QUERY =
+            "SELECT * FROM solutions where id = ?";
+    private static final String UPDATE_SOLUTION_QUERY =
+            "UPDATE solutions SET created = ?, updated = ?, description = ?, exercise_id = ?, user_id where id = ?";
+    private static final String DELETE_SOLUTION_QUERY =
+            "DELETE FROM solutions WHERE id = ?";
+    private static final String FIND_ALL_SOLUTION_QUERY =
+            "SELECT * FROM solutions";
+
+    public Solution create(Solution solution) {
+        try (Connection conn = DBUtil.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(CREATE_SOLUTION_QUERY, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, solution.getCreated());
+            statement.setString(2, solution.getUpdated());
+            statement.setString(3, solution.getDescription());
+            statement.setInt(4, solution.getExerciseId());
+            statement.setInt(5, solution.getUserId());
+            statement.executeUpdate();
+            ResultSet resultSet = statement.getGeneratedKeys();
+            if (resultSet.next()) {
+                solution.setId(resultSet.getInt(1));
+            }
+            return solution;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public Solution read(int solutionId) {
+        try (Connection conn = DBUtil.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(READ_SOLUTION_QUERY);
+            statement.setInt(1, solutionId);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                Solution solution = new Solution();
+                solution.setId(resultSet.getInt("id"));
+                solution.setCreated(resultSet.getString("created"));
+                solution.setUpdated(resultSet.getString("updated"));
+                solution.setDescription(resultSet.getString("description"));
+                solution.setExerciseId(resultSet.getInt("exercise_id"));
+                solution.setUserId(resultSet.getInt("user_id"));
+                return solution;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void update(Solution solution) {
+        try (Connection conn = DBUtil.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(UPDATE_SOLUTION_QUERY);
+            statement.setString(1, solution.getCreated());
+            statement.setString(2, solution.getUpdated());
+            statement.setString(3, solution.getDescription());
+            statement.setInt(4, solution.getExerciseId());
+            statement.setInt(5, solution.getUserId());
+            statement.setInt(6,solution.getId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void delete(int solutionId) {
+        try (Connection conn = DBUtil.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(DELETE_SOLUTION_QUERY);
+            statement.setInt(1, solutionId);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Solution[] findAll() {
+        try (Connection conn = DBUtil.getConnection()) {
+            Solution[] solutions = new Solution[0];
+            PreparedStatement statement = conn.prepareStatement(FIND_ALL_SOLUTION_QUERY);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                Solution solution = new Solution();
+                solution.setId(resultSet.getInt("id"));
+                solution.setCreated(resultSet.getString("created"));
+                solution.setUpdated(resultSet.getString("updated"));
+                solution.setDescription(resultSet.getString("description"));
+                solution.setExerciseId(resultSet.getInt("exercise_id"));
+                solution.setUserId(resultSet.getInt("user_id"));
+                solutions = addToArray(solution, solutions);
+            }
+            return solutions;
+        } catch (SQLException e) {
+            e.printStackTrace(); return null;
+        }
+    }
+
+    private Solution[] addToArray(Solution s, Solution[] solutions) {
+        Solution[] tmpSolutions = Arrays.copyOf(solutions, solutions.length + 1);
+        tmpSolutions[solutions.length - 1] = s;
+        return tmpSolutions;
+    }
+
+
+}
