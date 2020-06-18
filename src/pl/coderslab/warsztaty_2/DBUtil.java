993f7a9f16789e5fe259f434767ef8a46b6e5542
Pawel Wozniak <pawel.jan.wozniak@gmail.com>
2020-06-18 23:03:10 +0200

    Class DBUtil was corrected due to problem with the time zone

--- src/pl/coderslab/warsztaty_2/DBUtil.java
+++ src/pl/coderslab/warsztaty_2/DBUtil.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DBUtil {
 
-    private static final String DB_URL  = "jdbc:mysql://localhost:3306/warsztaty2?useSSL=false&characterEncoding=utf8";
+    private static final String DB_URL  = "jdbc:mysql://localhost:3306/warsztaty2?useSSL=false&characterEncoding=utf8&serverTimezone=UTC";
     private static final String DB_USER = "root";
     private static final String DB_PASS = "coderslab";
 
